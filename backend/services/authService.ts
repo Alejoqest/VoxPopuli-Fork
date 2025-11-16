4997fd52ac32fc83cc6deeb9124d5e584a2334ad
Alejoqest <carpetaestudiantefabianalejo@proton.me>
2025-11-16 09:54:40 -0300

    refactor: improved error messages in authService login and register

--- backend/services/authService.ts
+++ backend/services/authService.ts
@@ -25,12 +25,12 @@ export const authService = {
         });
 
         if (error) {
-            if (error.message.includes('User already registered')) return ('Ya existe un usuario con esas crendenciales')
+            if (error.message.includes('User already registered')) return ('Ya existe un usuario con esas crendenciales.')
             return error.message;
         }
 
         const user = data.user;
-        if (!user) return "User no retornado";
+        if (!user) return ("User no retornado");
 
         const i = Math.floor(Math.random() * 10);
         const color = chartColors[i];
@@ -47,7 +47,7 @@ export const authService = {
     login: async (text: string, password: string) => {
         const { data } = await supabase.from('profile').select('*').or(`email.eq.${text},username.eq.${text}`).single();
 
-        if (!data) return ("No hay usuario");
+        if (!data) return ("No hay usuario con esas crendenciales.");
 
         const { error: loginError } = await supabase.auth.signInWithPassword({
             email: data.email,
@@ -58,7 +58,7 @@ export const authService = {
             if (loginError.message.includes("Email not confirmed")) return ("Correo no verificado" + "\n" +
                 "Por favor, verifica tu correo electrónico antes de iniciar sesión.")
 
-            if (loginError.message.includes('Invalid login credentials')) return ('Contraseña equivocada');
+            if (loginError.message.includes('Invalid login credentials')) return ('Contraseña introducida es incorrecta.');
         }
 
         return loginError?.message || null;
